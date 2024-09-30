7b8b8e61588bb24e213a9efb0a4d9013e23463ed
hscho0048 <chohosung27@gmail.com>
2024-10-01 00:12:08 +0900

    Second commit

--- 계산기.py
+++ 계산기.py
@@ -2,9 +2,19 @@ def add(a, b):
     return a + b 
 def subtract(a, b):
     return a - b
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "0으로는 못 나눔"
+    return a / b
+
 
 x = int(input("첫번쨰 숫자를 입력하세요: "))
 y = int(input("두번째 숫자를 입력하세요: "))
 
+print(f"Multiplication: {multiply(x, y)}")
+print(f"Division: {divide(x, y)}")
 print(f"Sum: {add(x, y)}")
 print(f"Difference: {subtract(x, y)}")
